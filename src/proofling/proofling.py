e157c66b905bce3d3da1ff60a4c8c4fc4c36edef
EricLBuehler <ericlbuehler@gmail.com>
2023-04-06 05:44:44 -0400

    Reorganize code

--- src/proofling/proofling.py
+++ src/proofling/proofling.py
@@ -29,26 +29,27 @@ class Proofling:
             yield char
         proof_blocks.ParserState.set_last_proposition(False)
 
-    def __parse_lines(self, lines_strs: typing.List[str]) -> typing.List[proof_blocks.Block]:
-        lines: typing.List[proof_blocks.Block] = []
-        for line in lines_strs:
-            print(line) #DEBUG
-            line_gen = self.__line_generator(line)
-            lines.append(proof_blocks.parse_next(line_gen))
-            
-        return lines
-    
-    def check(self, proof: str) -> bool:
+    def __parse_lines(self, proof: str) -> typing.List[proof_blocks.Block]:
         # 1st, strip whitespace from all lines. 2nd, filter out empty lines
         lines_str = list(filter(lambda s: s!="", [s.strip() for s in proof.split("\n")]))
 
         if lines_str.index("{")<0 or lines_str.index("}")<0:
             raise errors.ParseError("Expected proper name declaration are bounded by { ... }")
 
-        lines = self.__parse_lines(lines_str[lines_str.index("}")+1:])
-        print(lines) #DEBUG
-
         names = self.__parse_names(lines_str[lines_str.index("{")+1:lines_str.index("}")])
         print(names) #DEBUG
+
+        lines: typing.List[proof_blocks.Block] = []
+        for line in lines_str[lines_str.index("}")+1:]:
+            print(line) #DEBUG
+            line_gen = self.__line_generator(line)
+            lines.append(proof_blocks.parse_next(line_gen))
+            
+        print(lines) #DEBUG
+            
+        return lines
+    
+    def check(self, proof: str) -> bool:
+        self.__parse_lines(proof)
 
         return True
