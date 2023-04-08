cdf70193aa4ac78c1f40c21cfd54d9f1b4ecdac1
EricLBuehler <ericlbuehler@gmail.com>
2023-04-08 05:57:47 -0400

    Add simple test and prep for large development

--- tests/test_proofs.py
+++ tests/test_proofs.py
@@ -1,16 +1,24 @@
 from proofling.proofling import Proofling
 
-def test_with_value():
-    print()
+def _test_with_value():
+    print("\n\nTest with value")
     with open("proof_value.txt", "r") as file:
         text = file.read()
 
     proofling = Proofling()
     assert proofling.check(text), "should be True"
 
-def test_with_no_value():
-    print()
+def _test_with_no_value():
+    print("\n\nTest with no value")
     with open("proof_no_value.txt", "r") as file:
+        text = file.read()
+        
+    proofling = Proofling()
+    assert proofling.check(text), "should be True"
+
+def test_simple_proof():
+    print("\n\nTest with simple proof")
+    with open("proof_simple.txt", "r") as file:
         text = file.read()
         
     proofling = Proofling()
