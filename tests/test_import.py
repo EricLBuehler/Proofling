76efb70d7624449761d5af2e20b9805b4105bce1
EricLBuehler <ericlbuehler@gmail.com>
2023-04-09 16:36:04 -0400

    Better tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,3 +1,6 @@
 def test_import_primary():
+    print("\n\nTest imports:")
     import proofling
     from proofling import proofling
+    import proofling.propositions
+    print("\033[1mTest imports: ✅")
--- tests/test_proofs.py
+++ tests/test_proofs.py
@@ -1,25 +1,28 @@
 from proofling.proofling import Proofling
 
-def _test_with_value():
-    print("\n\nTest with value")
+def test_with_value():
+    print("\n\nTest proof with proposition that has a value:")
     with open("proof_value.txt", "r") as file:
         text = file.read()
 
     proofling = Proofling()
     assert proofling.check(text), "should be True"
+    print("\033[1mTest proof with value: ✅")
 
-def _test_with_no_value():
-    print("\n\nTest with no value")
+def test_with_no_value():
+    print("\n\nTest proof with proposition that has no value:")
     with open("proof_no_value.txt", "r") as file:
         text = file.read()
         
     proofling = Proofling()
     assert proofling.check(text), "should be True"
+    print("\033[1mTest proof with proposition that has no value: ✅")
 
 def test_simple_proof():
-    print("\n\nTest with simple proof")
+    print("\n\nTest with simple proof:")
     with open("proof_simple.txt", "r") as file:
         text = file.read()
         
     proofling = Proofling()
     assert proofling.check(text), "should be True"
+    print("\033[1mTest simple proof: ✅")
