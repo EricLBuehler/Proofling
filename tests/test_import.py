39286ede4f77bea9c792fe0c7fd7501ffa47a3a3
EricLBuehler <ericlbuehler@gmail.com>
2023-04-10 13:36:48 -0400

    Minor update

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,6 +1,5 @@
 def test_import_primary():
     print("\n\nTest imports:")
-    import proofling
     from proofling import proofling
     import proofling.propositions
     print("✅ \033[1mTest imports")
